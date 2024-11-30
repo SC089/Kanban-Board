441ee01d79ca7482748aa15e6688c034396b5c93
Sebastian Casadevall <sacasadevall@gmail.com>
2024-11-30 03:54:05 -0500

    authAPI.tsc

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,8 +1,29 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
-const login = async (userInfo: UserLogin) => {
+const login = async (userInfo: UserLogin): Promise<string | null> => {
   // TODO: make a POST request to the login route
-}
+  try{
+    const response = await fetch('./auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error('Login failed:', errorData.message);
+      return null;
+    }
+
+    const data = await response.json();
+    return data.token;
+  } catch (error) {
+    console.error('Error during login:', error);
+    return null;
+  }
+};
 
 
 
