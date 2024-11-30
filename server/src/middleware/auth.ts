a237d5b5cebed50622156b2c2052a44f2d3c3368
Sebastian Casadevall <sacasadevall@gmail.com>
2024-11-30 03:24:51 -0500

    Auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -2,9 +2,26 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface JwtPayload {
+  id: string;
   username: string;
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access token missing or is invalid' });
+  }
+
+  try {
+    const secretKey = process.env.JWT_SECRET_KEY || 'secretkey';
+    const payload = jwt.verify(token, secretKey) as JwtPayload;
+
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid or expired token' });
+  }
 };
